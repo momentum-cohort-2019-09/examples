50ef9cdd850f5141db8ca995a7af5ad9f54b6775
Clinton Dreisbach <clinton@dreisbach.us>
2019-10-10 17:29:23 -0400

    Add all 20 rooms to HTW

--- w5d4--hunt-the-wumpus/cave.py
+++ w5d4--hunt-the-wumpus/cave.py
@@ -27,10 +27,15 @@ class Cave:
 
 
 CAVE_NAMES = [
-    "Airy Alcove", "Bat-filled Basement", "Charred Chasm", "Dank Dungeon",
+    "Airy Alcove", "Bat-Filled Basement", "Charred Chasm", "Dank Dungeon",
     "Eerie Eyelet", "Frightening Fissure", "Ghastly Grotto", "Heated Hellhole",
-    "Incendiary Interior", "Janky Jail", "Killer Kastle", "Lame Lounge"
+    "Incendiary Interior", "Janky Jail", "Killer Karst", "Lame Lounge",
+    "Meandering Mists", "Putrid Passage", "Sulfurous Sinkhole", "Terrible Tube",
+    "Undulating Underground", "Vertical Vastness", "Nasty Nest",
+    "Offal-Filled Opening"
 ]
 
-CAVE_CONNECTIONS = [[0, 1, 9], [1, 7], [2, 11, 4], [3, 10, 5], [4, 9, 6],
-                    [5, 8, 11], [6, 7], [8, 1]]
+CAVE_CONNECTIONS = [[0, 1, 4, 5], [1, 7, 2], [2, 9, 3], [3, 11, 4], [4, 13],
+                    [5, 6, 14], [6, 7, 16], [7, 8], [8, 9, 17], [9, 10],
+                    [10, 11, 18], [11, 12], [12, 13, 19], [13, 14], [15, 16],
+                    [16, 17], [18, 19]]
